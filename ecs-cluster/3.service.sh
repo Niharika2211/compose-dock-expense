3aeaae704739fd388ea4017b4f3f561afeba63b7
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-16 19:19:23 +0530

    create service for frontend

--- ecs-cluster/3.service.sh
+++ ecs-cluster/3.service.sh
@@ -9,11 +9,11 @@ aws ecs create-service \
     --network-configuration "awsvpcConfiguration={subnets=[subnet-01899a28d9cd091c2],securityGroups=[sg-0c2026150f42233ac],assignPublicIp=ENABLED}" \
     --service-registries "registryArn=arn:aws:servicediscovery:us-east-1:522814728660:service/srv-soi3bloshikrmdmx"
 
-# aws ecs create-service \
-#     --cluster expense \
-#     --service-name frontend-node-service \
-#     --task-definition expense-frontend \
-#     --desired-count 1 \
-#     --launch-type FARGATE \
-#     --network-configuration "awsvpcConfiguration={subnets=[subnet-01899a28d9cd091c2],securityGroups=[sg-0c2026150f42233ac],assignPublicIp=ENABLED}" \
-#     --service-registries "registryArn=arn:aws:servicediscovery:us-east-1:522814728660:service/srv-fb7ouzaf2kla3sx7"
+aws ecs create-service \
+    --cluster expense \
+    --service-name frontend-node-service \
+    --task-definition expense-frontend \
+    --desired-count 1 \
+    --launch-type FARGATE \
+    --network-configuration "awsvpcConfiguration={subnets=[subnet-01899a28d9cd091c2],securityGroups=[sg-0c2026150f42233ac],assignPublicIp=ENABLED}" \
+    --service-registries "registryArn=arn:aws:servicediscovery:us-east-1:522814728660:service/srv-fb7ouzaf2kla3sx7"
