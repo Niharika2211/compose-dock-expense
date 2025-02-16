25cdbc0b0c5dd63b8455a6bfd4c838e027b2ae3d
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-02-16 19:11:14 +0530

    update the script

--- ecs-cluster/2.td.sh
+++ ecs-cluster/2.td.sh
@@ -2,14 +2,16 @@
 
 echo "Uploading task definitions to the ECS cluster"
 
-
 scripts=("frontend-td.sh" "backend-td.sh")
-
 
 for script in "${scripts[@]}"; do
     if [[ -f "$script" ]]; then
         echo "Executing $script..."
         bash "$script"
+        if [[ $? -ne 0 ]]; then
+            echo "Error: $script execution failed!"
+            exit 1
+        fi
     else
         echo "Error: $script not found!"
     fi
